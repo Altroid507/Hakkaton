5db67455d2b6b69ab069f22b88af008a1378ffde
Altroid507 <125668171+Altroid507@users.noreply.github.com>
2023-10-10 16:28:36 +0300

    Update Hakkaton.c

--- Hakkaton.c
+++ Hakkaton.c
@@ -1,33 +1,20 @@
-long long step(int num, int step) {
-    int res;
-    res = num;
-    if (step == 0) return 1;
-    for (int i = 2; i <= step; i++) {
-        res = res * num;
+long long itc_bin_num(long long number) {
+    if (number > 4095) return 0;
+    if (number < 0) return 0;
+    long long res = 0;
+    long long base = 1;
+    while (number > 0) {
+      int d = number % 2;
+      res = d * base + res;
+      base = base * 10;
+      number = number / 2;
     }
     return res;
 }
 
-long long itc_bin_num(long long number) {
-    long long res;
-    res = 0;
-    if (number >= 2) {
-        for (int i = 0; number >= 2; i++) {
-            if (i != 0) {
-                res += (number % 2) * step(10, i);
-            }
-            else res += number % 2;
-            if ((number / 2) < 2) res += (number / 2) * step(10, i + 1);
-            number = number / 2;
-        }
-        return res;
-    }
-    return number;
-}
-
 long long binary_led(long long number){
   long long n = itc_bin_num(number);
-  for (int i = 2; i <= 11; i++){
+  for (int i = 2; i <= 13; i++){
     if (n % 10 == 1) digitalWrite(i, HIGH);
     n = n / 10;
   }
@@ -44,13 +31,15 @@ void setup() {
     pinMode(4, OUTPUT);
     pinMode(3, OUTPUT);
     pinMode(2, OUTPUT);
-    pinMode(1, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
 
 }
 void loop() {
     int n = Serial.parseInt();
     binary_led(n);
-    delay(3000);
+    delay(2000);
     digitalWrite(10, LOW);
     digitalWrite(9, LOW);
     digitalWrite(8, LOW);
@@ -60,5 +49,7 @@ void loop() {
     digitalWrite(4, LOW);
     digitalWrite(3, LOW);
     digitalWrite(2, LOW);
-    digitalWrite(1, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
 }
